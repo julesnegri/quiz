fedd91e5562e4ab0e21133320bdd68b152f54f3d
jules <jsoaresandrie@gmail.com>
2021-02-01 11:23:41 -0300

    ajuste app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,6 @@ import { createGlobalStyle, ThemeProvider } from 'styled-components';
 import Head from 'next/head';
 import db from '../db.json';
 
-
 const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
@@ -41,6 +40,34 @@ export default function App({ Component, pageProps }) {
       >
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
+
+        <meta property="og:title" content={db.title} />
+        <meta property="og:description" content={db.description} />
+        <meta property="og:image" content={db.bg} />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="theme-color" content="#ffffff" />
+
+        <meta name="title" content={db.title} />
+        <meta name="description" content={db.description} />
+
+        {/* <!-- Open Graph / Facebook --> */}
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://quiz.negri.vercel.app/" />
+        <meta property="og:title" content="Quiz da Massa - Supernatural" />
+        <meta property="og:description" content="" />
+        <meta property="og:image" content="/assets/background/img.jpg" />
+
+        {/* <!-- Twitter --> */}
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content="https://quiz.negri.vercel.app/" />
+        <meta property="twitter:title" content="Quiz da Massa - Supernatural" />
+        <meta property="twitter:description" content="" />
+        <meta property="twitter:image" content="/assets/background/img.jpg" />
+
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
